adaf061872769579d4744b306102cbfa5bb7c8f8
Fernando Wan-Dall <fernando@pensador.com.br>
2021-08-22 23:34:32 -0300

    Início da tela de contato

--- pages/contato.php
+++ pages/contato.php
@@ -1,1 +1,30 @@
 <h1>contato</h1>
+<div class="row mt-4">
+    <div class="col-lg-6 text-center order-lg-last">
+        <p>Telefones para contato e pedidos:</p>
+        <p>(99) 99999-9999</p>
+        <div class="col-lg-10 offset-lg-1 mt-md-5 border">
+            MAPA
+        </div>
+        <div class="border-bottom m-4 d-lg-none"></div>
+    </div>
+
+    <div class="col-lg-6 order-lg-first">
+        <form action="">
+            <div class="mb-3">
+                <label for="nome">Nome:</label>
+                <input type="text" class="form-control" id="nome" />
+            </div>
+            <div class="mb-3">
+                <label for="email">E-mail:</label>
+                <input type="text" class="form-control" id="email" />
+            </div>
+            <div class="mb-3">
+                <label for="mensagem">Mensagem:</label>
+                <textarea name="mensagem" id="mensagem" rows="3" class="form-control"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+    </div>
+</div>
